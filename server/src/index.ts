6607a7a89e8abd058906f099ff8036563d380bea
leoportogtr86 <leoporto_studio@hotmail.com>
2021-01-29 08:52:07 -0300

    Initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express()
+
+
+
+app.get('/', (req, res)=>{
+
+    return res.send('hello node + ts')
+
+
+})
+
+
+
+app.listen(8080, ()=>{
+
+    console.log('servidor')
+})
